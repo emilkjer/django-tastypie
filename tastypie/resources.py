3978a111dadc1af91073d7690bd653c320b542be
Emil Kjer <emil@kjer.info>
2012-12-19 13:48:18 +1100

    fix django 1.5

--- tastypie/resources.py
+++ tastypie/resources.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import patterns, url
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned, ValidationError
 from django.core.urlresolvers import NoReverseMatch, reverse, resolve, Resolver404, get_script_prefix
 from django.db import transaction
-from django.db.models.sql.constants import QUERY_TERMS, LOOKUP_SEP
+from django.db.models.sql.constants import QUERY_TERMS
 from django.http import HttpResponse, HttpResponseNotFound
 from django.utils.cache import patch_cache_control
 from tastypie.authentication import Authentication
@@ -39,6 +39,10 @@ try:
 except ImportError:
     def csrf_exempt(func):
         return func
+try:
+    from django.db.models.constants import LOOKUP_SEP
+except ImportError:
+    from django.db.models.sql.constants import LOOKUP_SEP
 
 
 class NOT_AVAILABLE:
